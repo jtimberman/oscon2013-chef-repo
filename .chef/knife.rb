3374fbeccbc059413b3c6717e52c2a1f89220f05
jtimberman <joshua@opscode.com>
2013-07-03 12:56:04 -0600

    add the node name

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -4,6 +4,7 @@ current_dir = File.dirname(__FILE__)
 # For Chef Server chef-zero. Change these for using a "real" Chef Server.
 chef_server_url "http://localhost:8889"
 client_key "#{current_dir}/zero.pem"
+node_name "zero"
 
 # cookbook_path is the location where knife looks for cookbooks
 cookbook_path ["#{current_dir}/../cookbooks"]
